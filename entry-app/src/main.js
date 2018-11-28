968f58ea0d27fa087307ba2625c4c73aedf91fdf
kkxujq <kkxujq@163.com>
2018-11-28 17:51:52 +0800

    :arrow_up: Upgrading code

--- entry-app/src/main.js
+++ entry-app/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import GlobalMethods from '@/utils/global-methods';
 
 Vue.config.productionTip = false;
+Vue.use(GlobalMethods);
 
 new Vue({
   router,
