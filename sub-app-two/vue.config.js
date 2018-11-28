468a98083e54613d803dc8cbb56b577a1c59a639
kkxujq <kkxujq@163.com>
2018-11-28 17:40:27 +0800

    :building_construction: Making architectural changes.

--- sub-app-two/vue.config.js
+++ sub-app-two/vue.config.js
@@ -1,109 +1,46 @@
-// const SrcCompileWebpackPlugin = require('webpack-gt-src-compile')
-const path = require('path');
-// const bourbon = require('bourbon').includePaths;
-// coust neat = require('bourbon-neat').includePaths;
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const ModifyChunkIdPlugin = require('modify-chunk-id-webpack-plugin');
+const APP_NAME = require('./package.json').name;
+const PORT = require('./package.json').devPort;
+const patchCliService = require('./scripts/patch-cli-service');
 
-function resolve(dir) {
-  return path.join(__dirname, '.', dir);
-}
+patchCliService();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+log('APP_NAME: ', APP_NAME);
+log('NODE_ENV: ', NODE_ENV);
 
 module.exports = {
-  devServer: {
-    // disableHostCheck: true,
-    port: 8888,
-    // open: process.platform === 'darwin',
-    host: 'localhost',
-    https: true,
-    hot: true,
-    hotOnly: true,
-    // before: app => {},
-    proxy: {
-      '/td': {
-        target: 'http://192.168.10.151:7081',
-        // target: 'https://192.168.10.130:17080',
-        // target: 'http://127.0.0.1:6666',
-        changeOrigin: true,
-        // pathRewrite: {
-        //   '^/td': '/td' // 目前暂不需要重写路径
-        // }
-      },
-    },
-  },
-  // 基本路径
-  baseUrl: '/',
-  // 输出文件目录
-  outputDir: 'dist',
-  // eslint-loader 是否在保存的时候检查
-  lintOnSave: true,
-  // 生产环境是否生成 sourceMap 文件
+  baseUrl: `/${APP_NAME}/`,
+
   productionSourceMap: false,
-  css: {
-    loaderOptions: {
-      // 给 sass-loader 传递选项
-      sass: {
-        data: '@import "@/style/variables.scss";',
-        // includePaths: [...bourbon, ...neat],
-      },
-    },
-  },
+
   configureWebpack: {
-    devtool: 'eval-source-map',
+    externals: {
+      vue: 'Vue',
+      'element-ui': 'ELEMENT',
+    },
+
+    entry: './src/module.js',
 
     output: {
-      devtoolModuleFilenameTemplate: info => info.resourcePath.match(/^\.\/\S*?\.vue$/)
-        ? `webpack-generated:///${info.resourcePath}?${info.hash}`
-        : `webpack-yourCode:///${info.resourcePath}`,
-
-      devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]',
+      libraryExport: 'default',
     },
 
-    resolve: {
-      mainFields: ['src:main', 'main'],
-    },
-
-    module: {
-      rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [path.join(__dirname, 'src'), /node_modules\/(pcms-components-.*)/],
-      }],
-    },
-    // plugins: [new SrcCompileWebpackPlugin()],
-    // plugins: [
-    //   new BundleAnalyzerPlugin(),
-    // ],
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.VUE_APP_NAME': JSON.stringify(APP_NAME),
+      }),
+      new ModifyChunkIdPlugin({ random: process.env.NODE_ENV === 'development' }),
+    ],
   },
-  // Workaround for
-  // - https://github.com/vuejs/vue-cli/issues/1351
-  // - https://github.com/jantimon/html-webpack-plugin/issues/870
-  // - https://github.com/jantimon/html-webpack-plugin/pull/953
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].chunksSortMode = 'none';
-      return args;
-    });
 
-    config.module.rules.delete('svg'); // 重点:删除默认配置中处理svg,
-    // const svgRule = config.module.rule('svg')
-    // svgRule.uses.clear()
-    config.module
-      .rule('svg-sprite-loader')
-      .test(/\.svg$/)
-      .include
-      .add(resolve('src/assets/svgIcons')) // 处理svg目录
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]',
-      });
-
-    config.resolve.alias
-      .set('Assets', resolve('src/assets'))
-      .set('Style', resolve('src/Style'));
+  devServer: {
+    port: PORT,
   },
-  transpileDependencies: [
-    /\bvue-awesome\b/,
-  ],
 };
+
+function log(label, content, options) {
+  console.log('\x1b[1m%s\x1b[31m%s\x1b[0m', label, content);
+}
